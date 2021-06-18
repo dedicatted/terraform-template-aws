cf4681fb78a298127eb924134ccbc85fe0c71bd6
georgethegreatat <unnamedgeorge@gmail.com>
2021-06-18 17:44:20 +0300

    Fix "is it possible to expose yaml to standalone yaml file?"

--- kubernetes-aws-template/application.tf
+++ kubernetes-aws-template/application.tf
@@ -6,12 +6,7 @@
 resource "kubectl_manifest" "game-2048-namespace" {
   count = var.install_2048_game ? 1 : 0
 
-  yaml_body = <<YAML
-apiVersion: v1
-kind: Namespace
-metadata:
-  name: game-2048
-YAML
+  yaml_body = file("kubernetes-aws-template/files/game-2048-namespace.yaml")
 
   wait       = true
   depends_on = [helm_release.aws_ingress]
@@ -20,29 +15,8 @@ YAML
 resource "kubectl_manifest" "game-2048-deployment" {
   count = var.install_2048_game ? 1 : 0
 
-  yaml_body =<<YAML
-apiVersion: apps/v1
-kind: Deployment
-metadata:
-  namespace: game-2048
-  name: deployment-2048
-spec:
-  selector:
-    matchLabels:
-      app.kubernetes.io/name: app-2048
-  replicas: 5
-  template:
-    metadata:
-      labels:
-        app.kubernetes.io/name: app-2048
-    spec:
-      containers:
-      - image: alexwhen/docker-2048
-        imagePullPolicy: Always
-        name: app-2048
-        ports:
-        - containerPort: 80
-YAML
+  yaml_body = file("kubernetes-aws-template/files/game-2048-deployment.yaml")
+
 
   wait       = true
   depends_on = [kubectl_manifest.game-2048-namespace]
@@ -51,21 +25,7 @@ YAML
 resource "kubectl_manifest" "game-2048-service" {
   count = var.install_2048_game ? 1 : 0
 
-  yaml_body =<<YAML
-apiVersion: v1
-kind: Service
-metadata:
-  namespace: game-2048
-  name: service-2048
-spec:
-  ports:
-    - port: 80
-      targetPort: 80
-      protocol: TCP
-  type: NodePort
-  selector:
-    app.kubernetes.io/name: app-2048
-YAML
+  yaml_body = file("kubernetes-aws-template/files/game-2048-service.yaml")
 
   wait       = true
   depends_on = [kubectl_manifest.game-2048-deployment]
@@ -74,25 +34,7 @@ YAML
 resource "kubectl_manifest" "game-2048-ingress" {
   count = var.install_2048_game ? 1 : 0
 
-  yaml_body =<<YAML
-apiVersion: extensions/v1beta1
-kind: Ingress
-metadata:
-  namespace: game-2048
-  name: ingress-2048
-  annotations:
-    kubernetes.io/ingress.class: alb
-    alb.ingress.kubernetes.io/scheme: internet-facing
-    alb.ingress.kubernetes.io/target-type: ip
-spec:
-  rules:
-    - http:
-        paths:
-          - path: /*
-            backend:
-              serviceName: service-2048
-              servicePort: 80
-YAML
+  yaml_body = file("kubernetes-aws-template/files/game-2048-ingress.yaml")
 
   wait       = true
   depends_on = [kubectl_manifest.game-2048-service]
